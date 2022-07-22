09b6ba8763764607c2e7a1e3e4a3dcfb040811ac
srajashekhar <udayarajashekhar@gmail.com>
2022-07-22 08:57:51 +0530

    Added: uc-1 program

--- src/com/bridgelabz/ticractoe/TicTacToe.java
+++ src/com/bridgelabz/ticractoe/TicTacToe.java
@@ -1,10 +1,27 @@
 package com.bridgelabz.ticractoe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+
+	static char[] board = new char[10];
+	char player, computer;
+	Scanner scan = new Scanner(System.in);
+
+	public void initialize() {
+		for (int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
+	}
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Tic Tac Toe program");
+		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+		System.out.println("---------");
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println("---------");
+		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+
 	}
 
 }
